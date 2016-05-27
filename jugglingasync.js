7cf06444ffc99003b43262a57e4e5b83a68aa8f6
Evan Li <evan.li.2010@hotmail.com>
2016-05-27 10:38:26 -0400

    Working solution implemented

--- jugglingasync.js
+++ jugglingasync.js
@@ -1,33 +1,36 @@
 var http = require('http');
 var urls = process.argv.slice(2);
 var counter = 0;
+var index = 0;
 
-var finalResults = [];
+var results = [];
 
 getUrls(urls);
 
 function getUrls(urls) {
-  urls.forEach(function(url) {
-    //get request to the url
-    http.get(url, function(response) {
+  for( var i = 0; i < urls.length; i++){
+    getHttp(urls[i], i);
+}
+
+function getHttp(url, index) {
+  http.get(url, function(response) {
+    var result = "";
+    response.setEncoding("utf8");
+    //add listener to the data event
+    //data event triggers multiple times
+    response.on("data", function(data) {
+      result += data;
+    });
+    response.on("end", function() {
+      // console.log(counter + " " + result);
+      results[index] = result;
       counter++;
-      var results = [];
-      response.setEncoding("utf8");
-      //add listener to the data event
-      //data event triggers multiple times
-      response.on("data", function(data) {
-        // console.log(counter);
-        results.push(data);
-      });
-      response.on("end", function() {
-        // console.log('done with ' + counter);
-        finalResults.push(results.join(''));
-        if(counter == 3) {
-          finalResults.forEach(function (result) {
-            console.log(result);
-          });
+      if(counter == 3) {
+        for(var i = 0; i < results.length; i++) {
+          console.log(results[i]);
         }
-      });
+      }
     });
   });
+}
 }
